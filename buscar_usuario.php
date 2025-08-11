e3c14efec095b2e9a704bae4dbacdd2b519e8e4a
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-08-11 17:14:31 -0300

    Create buscar_usuario.php
    Arquivo responsável por buscar usuários

---
+++ buscar_usuario.php
@@ -0,0 +1,70 @@
+<?php
+    session_start();
+    require_once 'conexao.php';
+
+    // Verifica se o usuario tem permissao de ADM ou Secretaria
+    if($_SESSION['perfil'] !=1 && $_SESSION['perfil'] !=2){
+        echo "<script>alert('Acesso Negado!');window.location.href='principal.php';</script>";
+        exit();
+    }
+    $usuario = []; // Inicializa a variavel para evitar erros
+
+    // Se o formulario for enviado busca o usuario pelo id ou nome
+    if($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['busca'])){
+        $busca = trim($_POST['busca']);
+
+        // Verifica se a busca é um numero ou um nome
+        if(is_numeric($busca)){
+            $sql = "SELECT * FROM usuario WHERE id_usuario = :busca ORDER BY nome ASC";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':busca', $busca, PDO::PARAM_INT);
+        }else{
+            $sql = "SELECT * FROM usuario WHERE nome LIKE :busca ORDER BY nome ASC";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindValue(':busca_nome', "%$busca%", PDO::PARAM_STR);
+        }
+    }else {
+        $sql = "SELECT * FROM usuario ORDER BY nome ASC";
+        $stmt = $pdo->prepare($sql);
+        
+    }
+        $stmt->execute();
+        $usuario = $stmt->fetchALL(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Usuário</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h2>Lista de Usuários</h2>
+    <form action="buscar_usuario.php" method="POST">
+        <label for="busca">Digite o ID ou Nome(opcional):</label>
+        <input type="text" id="busca" name="busca">
+
+        <button type="submit">Buscar</button>
+    </form>
+        <?php if(!empty($usuarios)): ?>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Perfil</th>
+                    <th>Ações</th>
+                </tr>
+                <?php foreach($usuarios as $usuario) :?>
+                    <tr>
+                    <td><?=htmlspecialchars($usuario['id_usuario'])?></td>
+                    <td><?=htmlspecialchars($usuario['nome'])?></td>
+                    <td><?=htmlspecialchars($usuario['email'])?></td>
+                    <td><?=htmlspecialchars($usuario['id_perfil'])?></td>
+                    <td><?=htmlspecialchars($usuario['id_usuario'])?></td>
+                </tr>
+            </table>
+            
+</body>
+</html>
