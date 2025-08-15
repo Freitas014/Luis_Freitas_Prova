eb42273bf1d9fec067c013e15182bc0b0ba33af5
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-08-15 17:07:19 -0300

    Update excluir_usuario.php
    Erros ajustados

--- excluir_usuario.php
+++ excluir_usuario.php
@@ -12,13 +12,13 @@ if($_SESSION['perfil'] !=1){
 $usuario = [];
 
 // Busca todos os usuarios cadastrados em ordem alfabetica.
-$sql = "SELECT * FROM usuarios ORDER BY nome ASC";
+$sql = "SELECT * FROM usuario ORDER BY nome ASC";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 //Se um id for passado via GET excluir o usuario
-if(isset($_GET['id']) && is_numeric($_GET['id']));
+if(isset($_GET['id']) && is_numeric($_GET['id'])){
     $id_usuario = $_GET['id'];
 
     // Excluir o usuario do banco de dados.
@@ -31,3 +31,48 @@ if(isset($_GET['id']) && is_numeric($_GET['id']));
     } else {
         echo "<script>alert('Erro ao excluir o usuario!');</script>";
     }
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Excluir Usuários</title>
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+<body>
+        <center><h2>Excluir Usuários</h2></center>
+        <?php if(!empty($usuarios)): ?>
+            <center>
+                <table class="table" border="2">
+                    <tr class="tr">
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Perfil</th>
+                        <th>Ações</th>
+                    </tr>
+                <?php foreach($usuarios as $usuario): ?>
+                    <tr>
+                        <td><center><?= htmlspecialchars($usuario['id_usuario'])?></center></td>
+                        <td><?= htmlspecialchars($usuario['nome'])?></td>
+                        <td><?= htmlspecialchars($usuario['email'])?></td>
+                        <td><center><?= htmlspecialchars($usuario['id_perfil'])?></center></td>
+                        <td>
+                            <a href="excluir_usuario.php?id=<?=htmlspecialchars($usuario['id_usuario'])?>" 
+                            onclick="return confirm('Tem certeza que deseja excluir este usuário?')"><center><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#a90000" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+</svg></i></a></center>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>    
+                </table>
+            </center>
+        <?php else: ?>
+            <p>Nenhum usuário encontrado.</p>
+        <?php endif ?>
+        <br>
+        <center><a href="principal.php">Voltar</a></center>
+</body>
+</html>
