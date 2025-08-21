c370a0eb51273ac201a9c202244513811c1537c3
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-08-21 17:06:49 -0300

    Update alterar_usuario.php
    adicionado DropDown para navegação.

--- alterar_usuario.php
+++ alterar_usuario.php
@@ -7,6 +7,56 @@
         echo "<script>alert('Acesso Negado!');window.location.href='principal.php';</script>";
         exit();
     }
+    // Obtendo o nome do perfil do usuario logado
+$id_perfil = $_SESSION['perfil'];
+$sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+$stmtPerfil = $pdo->prepare($sqlPerfil);
+$stmtPerfil ->bindParam(':id_perfil', $id_perfil);
+$stmtPerfil->execute();
+$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+$nome_perfil = $perfil['nome_perfil'];
+
+// Definição das permissoes por perfil
+
+$permissoes = [
+    1 => ["Cadastrar" =>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php",
+        "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
+
+        "Buscar" =>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php",
+        
+        "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
+        
+        "Alterar" =>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php",
+        "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
+
+        "Excluir" =>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php",
+        "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"],],
+
+    2 => ["Cadastrar" =>["cadastro_cliente.php"],
+
+        "Buscar" =>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+
+        "Alterar" =>["alterar_produto.php", "alterar_fornecedor"],
+
+        "Excluir" =>["excluir_produto.php"],],
+
+    3 => ["Cadastrar" =>["cadastro_funcionario.php"],
+
+        "Buscar" =>["buscar_cliente.php","buscar_fornecedor.php", "buscar_produto.php"],
+          
+        "Alterar" =>["alterar_fornecedor.php", "alterar_produto.php"],
+
+        "Excluir" =>["excluir_produto.php"],],
+
+    4 => ["Cadastrar" => ["cadastro_cliente.php"],
+
+        "Buscar" =>["buscar_produto.php"],
+
+        "Alterar" =>["alterar_cliente.php"],]
+    ];
+
+    //Obtendo as opçoes disponiveis para o perfil logado.
+    $opcoes_menu = $permissoes[$id_perfil];
 
     //inicializa variaveis.
     $usuario = null;
@@ -45,11 +95,28 @@
     <script src="scripts.js"></script>
     <!-- Certifique-se que o JavaScript está sendo carregado corretamente. -->
 </head>
+<nav>
+        <ul class="menu">
+            <?php 
+                foreach($opcoes_menu as $categoria => $arquivos): ?>
+                <li class="dropdown">
+                    <a href="#"><?=$categoria ?></a>
+                    <ul class="dropdown-menu">
+                        <?php foreach($arquivos as $arquivo): ?>
+                            <li>
+                                <a href="<?=$arquivo ?>"><?=ucfirst(str_replace("_"," ",basename($arquivo,".php")))?></a>
+                            </li>   
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
 <body>
     <h2>Alterar Usuário</h2>
     <form action="alterar_usuario.php" method="POST">
         <label for="busca_usuario">Digite o ID ou o nome do usuário:</label>
-        <input type="text" id="busca_usuario" name="busca_usuario" required onkeyup="buscarSugestoes()">
+        <input type="text" class="form-control" id="busca_usuario" name="busca_usuario" required onkeyup="buscarSugestoes()">
 
         <!-- Div para exibir sugestoes de usuarios -->
          <div id="sugestoes">
@@ -63,10 +130,10 @@
             <input type="hidden" name="id_usuario" value="<?=htmlspecialchars($usuario['id_usuario'])?>">
 
             <label for="nome">Nome:</label>
-            <input type="text" name="nome" id="nome" value="<?=htmlspecialchars($usuario['nome'])?>" required>
+            <input type="text" class="form-control" name="nome" id="nome" value="<?=htmlspecialchars($usuario['nome'])?>" required>
 
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" value="<?=htmlspecialchars($usuario['email']);?>" required>
+            <input type="email" class="form-control" id="email" name="email" value="<?=htmlspecialchars($usuario['email']);?>" required>
 
             <label for="id_perfil">Perfil:</label>
             <select id="id_perfil" name="id_perfil">
@@ -79,11 +146,11 @@
             <!-- Se o usuario logado for adm, exibir opcao de alterar senha -->
             <?php if ($_SESSION['perfil'] == 1): ?>
                 <label for="nova_senha">Nova Senha</label>
-                <input type="password" id="nova_senha" name="nova_senha">
+                <input type="password" class="form-control" id="nova_senha" name="nova_senha">
             <?php endif; ?>
         
-            <button type="submit">Alterar</button>
-            <button type="reset">Cancelar</button>
+            <button type="submit" class="btn btn-outline-warning">Alterar</button>
+            <button type="reset" class="btn btn-outline-warning">Cancelar</button>
         </form>
     <?php endif; ?>
     <a href="principal.php">Voltar</a>
