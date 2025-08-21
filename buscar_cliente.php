02c0e12a02b9edce263cb3985d447dc64a24b429
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-08-21 17:04:44 -0300

    Create buscar_cliente.php
    Arquivo responsável por buscar e listar os clientes

---
+++ buscar_cliente.php
@@ -0,0 +1,155 @@
+<?php
+    session_start();
+    require_once 'conexao.php';
+
+    // Verifica se o cliente tem permissao de ADM ou Secretaria
+    if($_SESSION['perfil'] !=1 && $_SESSION['perfil'] !=2){
+        echo "<script>alert('Acesso Negado!');window.location.href='principal.php';</script>";
+        exit();
+    }
+    // Obtendo o nome do perfil do usuario logado
+$id_perfil = $_SESSION['perfil'];
+$sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+$stmtPerfil = $pdo->prepare($sqlPerfil);
+$stmtPerfil ->bindParam(':id_perfil', $id_perfil);
+$stmtPerfil->execute();
+$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+$nome_perfil = $perfil['nome_perfil'];
+
+// Definição das permissoes por perfil
+
+$permissoes = [
+    1 => ["Cadastrar" =>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php",
+        "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
+
+        "Buscar" =>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php",
+        
+        "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
+        
+        "Alterar" =>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php",
+        "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
+
+        "Excluir" =>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php",
+        "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"],],
+
+    2 => ["Cadastrar" =>["cadastro_cliente.php"],
+
+        "Buscar" =>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+
+        "Alterar" =>["alterar_produto.php", "alterar_fornecedor"],
+
+        "Excluir" =>["excluir_produto.php"],],
+
+    3 => ["Cadastrar" =>["cadastro_funcionario.php"],
+
+        "Buscar" =>["buscar_cliente.php","buscar_fornecedor.php", "buscar_produto.php"],
+          
+        "Alterar" =>["alterar_fornecedor.php", "alterar_produto.php"],
+
+        "Excluir" =>["excluir_produto.php"],],
+
+    4 => ["Cadastrar" => ["cadastro_cliente.php"],
+
+        "Buscar" =>["buscar_produto.php"],
+
+        "Alterar" =>["alterar_cliente.php"],]
+];
+
+    //Obtendo as opçoes disponiveis para o perfil logado.
+    $opcoes_menu = $permissoes[$id_perfil];
+    $cliente = []; // Inicializa a variavel para evitar erros
+
+    // Se o formulario for enviado busca o cliente pelo id ou nome
+    if($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['busca'])){
+        $busca = trim($_POST['busca']);
+
+        // Verifica se a busca é um numero ou um nome
+        if(is_numeric($busca)){
+            $sql = "SELECT * FROM cliente WHERE id_cliente = :busca ORDER BY nome_cliente ASC";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':busca', $busca, PDO::PARAM_INT);
+        }else{
+            $sql = "SELECT * FROM cliente WHERE nome_cliente LIKE :busca_nome ORDER BY nome_cliente ASC";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindValue(':busca_nome', "$busca%", PDO::PARAM_STR);
+        }
+    }else {
+        $sql = "SELECT * FROM cliente ORDER BY nome_cliente ASC";
+        $stmt = $pdo->prepare($sql);
+        
+    }
+        $stmt->execute();
+        $clientes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Usuário</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+    <nav>
+        <ul class="menu">
+            <?php 
+                foreach($opcoes_menu as $categoria => $arquivos): ?>
+                <li class="dropdown">
+                    <a href="#"><?=$categoria ?></a>
+                    <ul class="dropdown-menu">
+                        <?php foreach($arquivos as $arquivo): ?>
+                            <li>
+                                <a href="<?=$arquivo ?>"><?=ucfirst(str_replace("_"," ",basename($arquivo,".php")))?></a>
+                            </li>   
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
+<body>
+    <h2>Lista de Usuários</h2>
+    <form action="buscar_cliente.php" method="POST">
+        <label for="busca">Digite o ID ou Nome(opcional):</label>
+        <input type="text" class="form-control" id="busca" name="busca">
+
+        <button type="submit" class="btn btn-outline-warning">Buscar</button>
+    </form>
+        <?php if(!empty($clientes)): ?>
+            <center>
+            <table class="table" border="1">
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Endereço</th>
+                    <th>Telefone</th>
+                    <th>Ações</th>
+                </tr>
+                <?php foreach($clientes as $cliente) :?>
+                <tr>
+                    <td><?=htmlspecialchars($cliente['id_cliente'])?></td>
+                    <td><?=htmlspecialchars($cliente['nome_cliente'])?></td>
+                    <td><?=htmlspecialchars($cliente['email'])?></td>
+                    <td><?=htmlspecialchars($cliente['endereco'])?></td>
+                    <td><?=htmlspecialchars($cliente['telefone'])?></td>
+                    <td>
+                    <a href="alterar_cliente.php><?=htmlspecialchars($cliente['id_cliente'])?>"><center><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#3d3d3d" id="alterar" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                      </svg></a></center>
+                            
+                        <a href="excluir_cliente.php><?=htmlspecialchars($cliente['id_cliente'])?>" onclick="return confirm('Tem certeza que deseja excluír este usuário?')"><center><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#a90000" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+                      </svg></i></a></center>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </table></center>
+        <?php else: ?>
+            <p>Nenhum usuário encontrado.</p>
+        <?php endif; ?>
+        <br>
+        <a href="principal.php">Voltar</a>
+</body>
+</html>
