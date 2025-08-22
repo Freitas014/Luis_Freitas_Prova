7078ec9c1b3f6ed8439ae234a9a9b6c0802e8c8e
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-08-22 17:04:01 -0300

    Update buscar_cliente.php
    Erro de sintaxe que impedia o redirecionameno para excluir.php e alterar.php foi resolvido.

--- buscar_cliente.php
+++ buscar_cliente.php
@@ -108,10 +108,10 @@ $permissoes = [
         </ul>
     </nav>
 <body>
-    <h2>Lista de Usuários</h2>
+    <h2>Lista de Cliente</h2>
     <form action="buscar_cliente.php" method="POST">
         <label for="busca">Digite o ID ou Nome(opcional):</label>
-        <input type="text" class="form-control" id="busca" name="busca">
+        <input type="text" class="form-control" placeholder="Insira ID ou Nome" id="busca" name="busca" required>
 
         <button type="submit" class="btn btn-outline-warning">Buscar</button>
     </form>
@@ -134,12 +134,12 @@ $permissoes = [
                     <td><?=htmlspecialchars($cliente['endereco'])?></td>
                     <td><?=htmlspecialchars($cliente['telefone'])?></td>
                     <td>
-                    <a href="alterar_cliente.php><?=htmlspecialchars($cliente['id_cliente'])?>"><center><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#3d3d3d" id="alterar" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <a href="alterar_cliente.php?id=<?=htmlspecialchars($cliente['id_cliente'])?>"><center><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#3d3d3d" id="alterar" class="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
                       </svg></a></center>
                             
-                        <a href="excluir_cliente.php><?=htmlspecialchars($cliente['id_cliente'])?>" onclick="return confirm('Tem certeza que deseja excluír este usuário?')"><center><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#a90000" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                        <a href="excluir_cliente.php?id=<?=htmlspecialchars($cliente['id_cliente'])?>" onclick="return confirm('Tem certeza que deseja excluír este usuário?')"><center><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#a90000" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
                       </svg></i></a></center>
                     </td>
@@ -147,7 +147,7 @@ $permissoes = [
                 <?php endforeach; ?>
             </table></center>
         <?php else: ?>
-            <p>Nenhum usuário encontrado.</p>
+            <p>Nenhum cliente encontrado.</p>
         <?php endif; ?>
         <br>
         <a href="principal.php">Voltar</a>
