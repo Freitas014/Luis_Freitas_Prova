5675805b09bbcd566cd32ed533350f087b9881ec
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-08-22 17:07:41 -0300

    Update excluir_usuario.php
    Adicionado Menu DropDown para navegação

--- excluir_usuario.php
+++ excluir_usuario.php
@@ -7,6 +7,56 @@ if($_SESSION['perfil'] !=1){
     echo"<script>alert('Acesso Negado!');window.location.href='principal.php'</script>";
     exit();
 }
+// Obtendo o nome do perfil do usuario logado
+$id_perfil = $_SESSION['perfil'];
+$sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+$stmtPerfil = $pdo->prepare($sqlPerfil);
+$stmtPerfil ->bindParam(':id_perfil', $id_perfil);
+$stmtPerfil->execute();
+$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+$nome_perfil = $perfil['nome_perfil'];
+
+// Definição das permissoes por perfil
+
+$permissoes = [
+    1 => ["Cadastrar" =>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php",
+        "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
+
+        "Buscar" =>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php",
+        
+        "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
+        
+        "Alterar" =>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php",
+        "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
+
+        "Excluir" =>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php",
+        "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"],],
+
+    2 => ["Cadastrar" =>["cadastro_cliente.php"],
+
+        "Buscar" =>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+
+        "Alterar" =>["alterar_produto.php", "alterar_fornecedor"],
+
+        "Excluir" =>["excluir_produto.php"],],
+
+    3 => ["Cadastrar" =>["cadastro_funcionario.php"],
+
+        "Buscar" =>["buscar_cliente.php","buscar_fornecedor.php", "buscar_produto.php"],
+          
+        "Alterar" =>["alterar_fornecedor.php", "alterar_produto.php"],
+
+        "Excluir" =>["excluir_produto.php"],],
+
+    4 => ["Cadastrar" => ["cadastro_cliente.php"],
+
+        "Buscar" =>["buscar_produto.php"],
+
+        "Alterar" =>["alterar_cliente.php"],]
+];
+
+    //Obtendo as opçoes disponiveis para o perfil logado.
+    $opcoes_menu = $permissoes[$id_perfil];
 
 // Inicializa variavel por armazenas usuarios.
 $usuario = [];
@@ -40,7 +90,25 @@ if(isset($_GET['id']) && is_numeric($_GET['id'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Excluir Usuários</title>
     <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
+    <nav>
+        <ul class="menu">
+            <?php 
+                foreach($opcoes_menu as $categoria => $arquivos): ?>
+                <li class="dropdown">
+                    <a href="#"><?=$categoria ?></a>
+                    <ul class="dropdown-menu">
+                        <?php foreach($arquivos as $arquivo): ?>
+                            <li>
+                                <a href="<?=$arquivo ?>"><?=ucfirst(str_replace("_"," ",basename($arquivo,".php")))?></a>
+                            </li>   
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
 <body>
         <center><h2>Excluir Usuários</h2></center>
         <?php if(!empty($usuarios)): ?>
