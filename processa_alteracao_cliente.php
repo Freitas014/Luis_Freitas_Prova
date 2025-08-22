c7f5fa97353e7c4b767fee6d482f2dc9fc3b9c1c
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-08-22 17:05:53 -0300

    Update processa_alteracao_cliente.php

--- processa_alteracao_cliente.php
+++ processa_alteracao_cliente.php
@@ -2,35 +2,37 @@
     session_start();
     require 'conexao.php';
 
-    if($_SESSION['perfil'] !=1){
+    if($_SESSION['perfil'] !=1 && $_SESSION['perfil'] !=4){
         echo "<script>alert('Acesso Negado!');window.location.href='principal.php';</script>";
         exit();
     }
 
     if($_SERVER["REQUEST_METHOD"] == "POST");
-    $id_cliente = $_POST['id_cliente'];
-    $nome_cliente = $_POST['nome_cliente'];
+    $id = $_POST['id_cliente'];
+    $nome = $_POST['nome_cliente'];
     $email = $_POST['email'];
     $endereco = $_POST['endereco'];
     $telefone = $_POST['telefone'];
+    // $novos_dados = $_POST['nome_cliente'] &&  $_POST['email']  &&  $_POST['endereco'] &&  $_POST['telefone'];
 
-    // Atualiza os dados do usuario 
-    if($id_cliente && $nome_cliente && $email && $endereco && $telefone){
-        $sql = "UPDATE usuario SET nome_cliente = :nome_cliente, email = :email, endereco = :endereco, telefone = :telefone WHERE id_cliente = :id";
+    // Atualiza os dados do cliente 
+    if($nome_cliente && $email && $endereco && $telefone){
+        $sql = "UPDATE cliente SET nome_cliente = :nome_cliente, email = :email, endereco = :endereco, telefone = :telefone WHERE id_cliente = :id";
         $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(':telefone', $telefone);
+        $stmt->bindParam(':nome_cliente', $nome_cliente && ':email', $email && ":endereco", $endereco && ':telefone', $telefone);
     } else{
-        $sql = "UPDATE usuario SET nome_cliente = :nome_cliente, email = :email, endereco = :endereco, telefone = :telefone WHERE id_cliente = :id";
+        $sql = "UPDATE cliente SET nome_cliente = :nome_cliente, email = :email, endereco = :endereco, telefone = :telefone WHERE id_cliente = :id";
         $stmt = $pdo->prepare($sql);
     }
-    $stmt->bindParam(':nome_cliente', $nome_cliente);
+    $stmt->bindParam(':nome_cliente', $nome);
     $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':id_perfil', $id_perfil);
-    $stmt->bindParam(':id', $id_usuario);
+    $stmt->bindParam(':endereco', $endereco) ;
+    $stmt->bindParam(':telefone', $telefone);
+    $stmt->bindParam(':id', $id);
 
     if($stmt->execute()) {
-        echo "<script>alert('Cliente atualizado com sucesso!');window.location.href='buscar_usuario.php';</script>";
+        echo "<script>alert('Cliente atualizado com sucesso!');window.location.href='buscar_cliente.php';</script>";
     } else{
-        echo "<script>alert('Erro ao atualizar usuario!');window.location.href='alterar_usuario.php?=$id_usuario';</script>";
+        echo "<script>alert('Erro ao atualizar cliente!');window.location.href='alterar_cliente.php?=$id_cliente';</script>";
     }
 ?>
